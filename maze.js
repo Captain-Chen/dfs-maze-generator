4b69efc9124ae39e96603b3f1cf15a138857c390
Richard Nguyen <almachy@gmail.com>
2023-12-12 14:38:44 -0600

    General code refactoring * Changed function classes to actual classes * Added clear function though I assume the way each cell is rendered, this doesn't affect the drawing * Code reorganization:     - Classes are now defined at the beginning     - Grouped variables together

--- maze.js
+++ maze.js
@@ -3,153 +3,30 @@
 	// global keyword 'print' where 'this' is bound to 'console'
 	const print = console.log.bind(console);
 
-	// const variables
-	const SCREENWIDTH = 25;
-	const SCREENHEIGHT = 25;
-
-	const Wall = {
-		TOP: 0,
-		RIGHT: 1,
-		BOTTOM: 2,
-		LEFT: 3
-	};
-
-	// canvas properties
-	let pixelSize = 10;
-	let scaleFactor = 2;
-	let offSet = 2;
-	let c = document.getElementById("canvas");
-
-	// scale canvas to the size of the screen
-	c.width = SCREENWIDTH * pixelSize * scaleFactor + (offSet * 2);
-	c.height = SCREENHEIGHT * pixelSize * scaleFactor + (offSet * 2);
-
-	let ctx = c.getContext("2d");
-	ctx.strokeStyle = "violet";
-	ctx.scale(scaleFactor, scaleFactor);
-
-	// Cell "class"
-	function Cell(i, j) {
-		this.i = i;
-		this.j = j;
-		this.walls = [true, true, true, true]; // top-right-bottom-left
-		this.visited = false;
-	}
-
-	Cell.prototype.checkNeighbours = function () {
-		let neighbours = [];
-		for (let i = 0; i < offSets.length; i++) {
-			// check if it is in bounds
-			if (isInBounds(this.i + offSets[i].x, this.j + offSets[i].y)) {
-				const nextCell = screen.get(this.i + offSets[i].x, this.j + offSets[i].y);
-				if (!nextCell.visited) {
-					neighbours.push(nextCell);
+	class Screen {
+		constructor(width, height) {
+			this.running = true;
+			this.grid = new Array(width * height);
+			// populate grid with cells
+			for (let y = 0; y < SCREENHEIGHT; y++) {
+				for (let x = 0; x < SCREENWIDTH; x++) {
+					let cell = new Cell(x, y);
+					this.set(x, y, cell);
 				}
 			}
 		}
 
-		if (neighbours.length > 0) {
-			let random = Math.floor(Math.random() * neighbours.length);
-			return neighbours[random];
-		} else {
-			return;
-		}
-	};
-
-	Cell.prototype.highlight = function () {
-		let offSet = 1;
-		let x = this.i * pixelSize + offSet;
-		let y = this.j * pixelSize + offSet;
-		ctx.fillStyle = "violet";
-		ctx.fillRect(x, y, pixelSize, pixelSize);
-	}
-
-	Cell.prototype.show = function () {
-		let offSet = 1;
-		let x = this.i * pixelSize + offSet;
-		let y = this.j * pixelSize + offSet;
-
-		if (this.walls[Wall.TOP]) {
-			// top
-			ctx.beginPath();
-			ctx.moveTo(x, y);
-			ctx.lineTo(x + pixelSize, y);
-			ctx.closePath();
-			ctx.stroke();
-		}
-
-		if (this.walls[Wall.RIGHT]) {
-			// right
-			ctx.beginPath();
-			ctx.moveTo(x + pixelSize, y);
-			ctx.lineTo(x + pixelSize, y + pixelSize);
-			ctx.closePath();
-			ctx.stroke();
-		}
-
-		if (this.walls[Wall.BOTTOM]) {
-			// bottom
-			ctx.beginPath();
-			ctx.moveTo(x + pixelSize, y + pixelSize);
-			ctx.lineTo(x, y + pixelSize);
-			ctx.closePath();
-			ctx.stroke();
-		}
-
-		if (this.walls[Wall.LEFT]) {
-			// left
-			ctx.beginPath();
-			ctx.moveTo(x, y + pixelSize);
-			ctx.lineTo(x, y);
-			ctx.closePath();
-			ctx.stroke();
-		}
-
-		if (this.visited) {
-			ctx.fillStyle = "purple";
-			ctx.fillRect(x, y, pixelSize, pixelSize);
-		}
-	};
-
-	function Point(x, y) {
-		this.x = x;
-		this.y = y;
-	}
-
-	let offSets = [
-		new Point(0, -1),
-		new Point(1, 0),
-		new Point(0, 1),
-		new Point(-1, 0)
-	];
-
-	function Screen(width, height) {
-		this.running = true;
-		this.grid = new Array(width * height);
-		// populate grid with cells
-		for (let j = 0; j < SCREENHEIGHT; j++) {
-			for (let i = 0; i < SCREENWIDTH; i++) {
-				let cell = new Cell(i, j);
-				this.set(i, j, cell);
-			}
-		}
-	}
-
-	Screen.prototype = {
-		set: function (x, y, val) {
+		set(x, y, val) {
 			if (isInBounds(x, y)) {
 				this.grid[y * SCREENWIDTH + x] = val;
 			}
-		},
-		get: function (x, y) {
+		}
+
+		get(x, y) {
 			return this.grid[y * SCREENWIDTH + x];
-		},
-		drawText: function (x, y, text) {
-			for (let i = 0; i < text.length; i++) {
-				this.set(x + i, y, text[i]);
-			}
-		},
-		update: function () {
+		}
+
+		update() {
 			if (!current.visited) {
 				current.visited = true;
 			}
@@ -165,9 +42,10 @@
 			} else {
 				this.running = false;
 			}
-		},
-		render: function (context) {
-			let offSet = 0.5;
+		}
+
+		render() {
+			this.clear();
 			for (let y = 0; y < SCREENHEIGHT; y++) {
 				for (let x = 0; x < SCREENWIDTH; x++) {
 					this.get(x, y).show();
@@ -175,11 +53,141 @@
 			}
 			current.highlight();
 		}
+
+		clear() {
+			ctx.clearRect(0, 0, c.width, c.height);
+		}
 	};
 
+	class Point{
+		constructor(x, y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	class Cell {
+		constructor(x, y) {
+			this.x = x;
+			this.y = y;
+			this.walls = [true, true, true, true]; // top, right, bottom, left
+			this.visited = false;
+		}
+
+		checkNeighbours() {
+			let neighbours = [];
+			for (let i = 0; i < offSets.length; i++) {
+				// first check if it is in bounds before doing anything
+				if (isInBounds(this.x + offSets[i].x, this.y + offSets[i].y)) {
+					const nextCell = screen.get(this.x + offSets[i].x, this.y + offSets[i].y);
+					if (!nextCell.visited) {
+						neighbours.push(nextCell);
+					}
+				}
+			}
+
+			if (neighbours.length > 0) {
+				let random = Math.floor(Math.random() * neighbours.length);
+				return neighbours[random];
+			} else {
+				return;
+			}
+		}
+
+		highlight() {
+			let x = this.x * pixelSize;
+			let y = this.y * pixelSize;
+			ctx.save();
+				ctx.fillStyle = "violet";
+				ctx.fillRect(x, y, pixelSize, pixelSize);
+			ctx.restore();
+		}
+
+		show() {
+			let x = this.x * pixelSize;
+			let y = this.y * pixelSize;
+
+			ctx.beginPath();
+			if (this.walls[Wall.TOP]) {
+				// top
+				ctx.moveTo(x, y);
+				ctx.lineTo(x + pixelSize, y);
+			}
+
+			if (this.walls[Wall.RIGHT]) {
+				// right
+				ctx.moveTo(x + pixelSize, y);
+				ctx.lineTo(x + pixelSize, y + pixelSize);
+			}
+
+			if (this.walls[Wall.BOTTOM]) {
+				// bottom
+				ctx.moveTo(x + pixelSize, y + pixelSize);
+				ctx.lineTo(x, y + pixelSize);
+			}
+
+			if (this.walls[Wall.LEFT]) {
+				// left
+				ctx.moveTo(x, y + pixelSize);
+				ctx.lineTo(x, y);
+			}
+
+			if (this.visited) {
+				ctx.save();
+					ctx.fillStyle = "purple";
+					ctx.fillRect(x, y, pixelSize, pixelSize);
+				ctx.restore();
+			}
+			ctx.closePath();
+			ctx.stroke();
+		}
+	}
+
+	// modify these values to change the shape of the canvas
+	const SCREENWIDTH = 23;
+	const SCREENHEIGHT = 30;
+
+	const Wall = {
+		TOP: 0,
+		RIGHT: 1,
+		BOTTOM: 2,
+		LEFT: 3
+	};
+
+	// canvas properties
+	let pixelSize = 8;
+	let scaleFactor = 2;
+	let c = document.getElementById("canvas");
+
+	// scale canvas to size of screen
+	c.width = SCREENWIDTH * pixelSize * scaleFactor;
+	c.height = SCREENHEIGHT * pixelSize * scaleFactor;
+
+	let ctx = c.getContext("2d");
+	ctx.strokeStyle = "violet";
+	ctx.scale(scaleFactor, scaleFactor);
+	
+	// create the screen
+	let screen = new Screen(SCREENWIDTH, SCREENHEIGHT);
+	let fps = 30;
+
+	let current = screen.get(0, 0); // Set our initial starting point
+	let stack = []; // We will be managing our own stack
+
+	let stepsLabel = document.getElementById('steps');
+	let stepCount = 0;
+
+	let offSets = [
+		new Point(0, -1),
+		new Point(1, 0),
+		new Point(0, 1),
+		new Point(-1, 0)
+	];
+
+	// helper functions
 	function removeWalls(a, b) {
-		let x = a.i - b.i;
-		let y = a.j - b.j;
+		let x = a.x - b.x;
+		let y = a.y - b.y;
 
 		if (x === 1) {
 			a.walls[Wall.LEFT] = false;
@@ -191,7 +199,7 @@
 
 		if (y === 1) {
 			a.walls[Wall.TOP] = false;
-			b.walls[Wall.BOTTOM] = false;
+			b.walls[Wall.BOTTOM] = false; 
 		} else if (y === -1) {
 			a.walls[Wall.BOTTOM] = false;
 			b.walls[Wall.TOP] = false;
@@ -202,21 +210,14 @@
 		return x < SCREENWIDTH && x >= 0 && y < SCREENHEIGHT && y >= 0;
 	}
 
-	// create screen
-	let screen = new Screen(SCREENWIDTH, SCREENHEIGHT);
-	let fps = 30;
-
-	// initial starting point
-	let current = screen.get(0, 0);
-	let stack = [];
-
 	(function mainLoop() {
 		screen.update();
-		screen.render(ctx);
+		screen.render();
 		if (screen.running) {
 			setTimeout(function () {
+				stepsLabel.innerHTML = stepCount++;
 				requestAnimationFrame(mainLoop);
 			}, 1000 / fps);
 		}
 	})();
-})();
+})();  
